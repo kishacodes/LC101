47aa9249513a481e8ba135f4474a8519bd5044cb
Kisha Codes <Kishacodes@gmail.com>
2020-05-03 15:57:16 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = null;
+let paragraph = null;
+let missionAbort = null;
+
+function init() {
+  missionAbort = document.getElementById("abortMission");
+  button = document.getElementById("liftoff");
+  paragraph = document.querySelector("p");
+
+  // Put your code for the exercises here.
+}
+
+window.onload = init;
