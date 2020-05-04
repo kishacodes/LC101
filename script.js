cecd7243f6e34cb2225329fdb440f975dda3f2bf
Kisha Codes <Kishacodes@gmail.com>
2020-05-03 21:46:25 -0400

    Finished part 1 & part 3

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ let missionAbort = null;
 
 function init() {
   missionAbort = document.getElementById("abortMission");
-  button = document.getElementById("liftoff");
-  paragraph = document.querySelector("p");
+  missionAbort.addEventListener("mouseover", function (event) {
+    document.getElementById(
+      "abortMission"
+    ).onmouseover = missionAbort.style.background = "red";
+  });
+  missionAbort.addEventListener("click", function (event) {
+    document.getElementById("abortMission") = confirm(
+      "Are you sure you want to abort the mission?"
+    );
+  });
 
-  // Put your code for the exercises here. 3
+  button = document.getElementById("liftoff");
+  button.addEventListener("click", function (event) {
+    document.getElementById("main-text").innerHTML =
+      " Houston, we have lift off!";
+  });
+  paragraph = document.querySelector("p");
 }
 
 window.onload = init;
