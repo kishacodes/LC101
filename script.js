4878264e7e17bcad55eff4c1bd4540109dbf56b8
Kisha Codes <Kishacodes@gmail.com>
2020-05-04 20:17:10 -0400

    Part 3 completed

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+/* I didn't need to use the paragraph variable because i made the <p> an id
+Part 1: When "Take off" button is clicked, change the test to Houston, we have lift off!
+Part 2: When mouseover Abort Mission button, the button's background turns red.
+Part 3: When user clocks Abort Mission button, confirmation window appears Are you sure you want to abort.
+*/
 let button = null;
 let paragraph = null;
 let missionAbort = null;
 
 function init() {
   missionAbort = document.getElementById("abortMission");
-  missionAbort.addEventListener("mouseover", function (event) {
-    document.getElementById(
-      "abortMission"
-    ).onmouseover = missionAbort.style.background = "red";
+  missionAbort.addEventListener("mouseover", function () {
+    missionAbort.style.background = "red";
   });
-  missionAbort.addEventListener("click", function (event) {
-    document.getElementById("abortMission") = confirm(
-      "Are you sure you want to abort the mission?"
-    );
+
+  missionAbort.addEventListener("click", function () {
+    confirm("Are you sure you want to abort the mission?");
   });
 
   button = document.getElementById("liftoff");
